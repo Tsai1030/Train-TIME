abca944bafd8f55f8ad58c5468c6c427e4bc8ce4
Tsai1030 <pijh102511@gmail.com>
2026-06-12 11:24:23 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,27 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 600,
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'react-vendor',
+              test: /node_modules[\\/](react|react-dom)[\\/]/,
+              priority: 2,
+            },
+            {
+              name: 'three-vendor',
+              test: /node_modules[\\/]three[\\/]/,
+              priority: 1,
+            },
+          ],
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       '/tdx-auth': {
